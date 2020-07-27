99f8bb49131a1bf8bc2096a7086ef44fea922847
woody <68537181+woody256@users.noreply.github.com>
2020-07-27 02:36:59 -0500

    Update sum.rs

--- sum.rs
+++ sum.rs
@@ -2,6 +2,13 @@ fn sum(a: i32, b: i32) -> i32 {
     return a+b;
 }
 
+fn update(mut arr:[i32;3]){
+   for i in 0..3 {
+      arr[i] = 0;
+   }
+   println!("Inside update {:?}",arr);
+}
+
 fn main() {
     println!("Hello, world!");
     let result = sum(1, 2);
